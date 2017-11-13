0bb00da402d83f177f9dafc87b550d0f54c0f18f
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2017-11-13 16:02:47 +0700

    Add Flask as backend

---
+++ web_project/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def main():
+    app.run(debug=True, host='127.0.0.1')
+
+if __name__ == '__main__':
+    main()
+
+
