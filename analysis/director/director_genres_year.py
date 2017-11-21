a45cda5e0c7f14af95479c383b8e7c5ce0751a0c
Ratanon Jantanasukon <60070079@kmitl.ac.th>
2017-11-21 18:11:48 +0700

    link page

--- analysis/director/director_genres_year.py
+++ analysis/director/director_genres_year.py
@@ -5,14 +5,14 @@ import pygal
 import os
 from pygal.style import NeonStyle
 
-with open('check.json') as genres:
+with open('..\\..\\web_project\\check.json') as genres:
     DATA = json.load(genres) #import json file
 
 
 def read():
     """function read file csv"""
-    return pd.read_csv('..\\dataset\\tmdb_5000_movies.csv'),\
-    pd.read_csv('..\\dataset\\tmdb_5000_credits.csv')
+    return pd.read_csv('..\\..\\dataset\\tmdb_5000_movies.csv'),\
+    pd.read_csv('..\\..\\dataset\\tmdb_5000_credits.csv')
 
 
 def memory_director():
@@ -29,35 +29,27 @@ def memory_director():
     return check_dict, df_movie, df_credits
 
 
-def analyse(director_name, count):
+def analyse(director_name, year, mem, count):
     """function analyse"""
     director, df_movie, df_credits = memory_director()
-    check = {} #dictionary movie id and genres
-    genres = {} #check genres of director
     for idx, data in df_movie.iterrows():
-        check_list = []
-        for i in json.loads(data.genres):
-            check_list.append(i['name'])
-        check[data.id] = check_list
-    for i in director.keys():
-        if i == director_name:
-            for j in director[i]:
-                for k in check[j]:
-                    count += 1
-                    if k not in genres:
-                        genres[k] = 1
-                    else:
-                        genres[k] += 1
-    return genres, count
-    """genres is static genres of director
-    and director_name is input name director"""
+        check_year = str(data.release_date)[-1:-5:-1][::-1]
+        if data.id in director[director_name] and 0 <= year - int(check_year) <= 5:
+            for i in json.loads(data.genres):
+                if i['name'] not in mem:
+                    mem[i['name']] = float(data.popularity)
+                    count += float(data.popularity)
+                else:
+                    mem[i['name']] += float(data.popularity)
+                    count += float(data.popularity)
+    print(mem, count)
 
-def plotgraph(name):
+analyse(input(), int(input()), {}, 0)
+'''def plotgraph(name):
     """function plotgraph"""
-    pie_director = pygal.Pie(fill=True, interpolate='cubic', style=NeonStyle)
+    pie_director = pygal.Bar(fill=True, interpolate='cubic', style=NeonStyle)
     genres, count = analyse(name, 0)
     pie_director.title = 'Static Genres of '+name+' directing.(%)'
     for i in genres:
         pie_director.add(i, round(genres[i]/count*100, 3))
-    pie_director.render_to_file('../web_project/static/svg/director_genres.svg')
-
+    pie_director.render_to_file('../web_project/static/svg/director_genres.svg')'''
