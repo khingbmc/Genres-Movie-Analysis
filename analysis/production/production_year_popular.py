45cdcbac66e788994fba20cae42d4af7b2b5b3d1
Ratanon Jantanasukon <60070079@kmitl.ac.th>
2017-12-11 08:14:58 +0700

    .

--- analysis/production/production_year_popular.py
+++ analysis/production/production_year_popular.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 import pygal
-from pygal.style import CleanStyle
+from pygal.style import NeonStyle
 
 with open('..\\..\\web_project\\check.json') as genres:
     DATA = json.load(genres) #import json file
@@ -17,7 +17,7 @@ def analyse(production, year, mem):
     for idx, data in df_movie.iterrows():
         check_year = str(data.release_date)[-1:-5:-1][::-1]
         for i in json.loads(data.production_companies):
-            if i['name'] == production and 0 <= year-int(check_year) <= 5:
+            if i['name'] == production and 0 <= int(check_year)-year <= 5:
                 for i in json.loads(data.genres):
                     if i['name'] not in mem.keys():
                         mem[i['name']] = float(data.popularity)
@@ -28,14 +28,21 @@ def analyse(production, year, mem):
                     
     return mem, count
 
-def plotgraph(production, year):
+def plotgraph(production, year, check):
     """This function is plotgraph"""
     memory, count = analyse(production, year, {})
-    bar = pygal.Bar(fill=True, interpolate='cubic', style=CleanStyle)
-    bar.title = production+' genres popularity static in '+str(year-5)+'-'+str(year)
+    bar = pygal.Bar(fill=True, interpolate='cubic', style=NeonStyle)
+    bar.title = production+' genres popularity static in '+str(year)+'-'+str(year+5)
     check_mem = [memory[x] for x in memory.keys()]
     check_mem = sorted(check_mem)
-    for i in memory.keys(): 
-        bar.add(i, round(memory[i]/count*100, 3))
+    for i in memory.keys():
+        if memory[i] not in check:
+            check[memory[i]] = [i]
+        else:
+            check[memory[i]].append(i)
+    keys = sorted(check.keys())
+    for i in keys:
+        for j in check[i]:
+            bar.add(j, round(i/count*100, 3))
     bar.render_to_file('..\..\web_project\static\svg\production\%s\%s_%s.svg'%(production.replace(' ', '_'), production.replace(' ', '_'), year))
-plotgraph(input(), int(input()))
+plotgraph(input(), int(input()), {})
