90c347ce6a52373a6d7f5686d659c4b45338de8c
Ratanon Jantanasukon <60070079@kmitl.ac.th>
2017-12-12 16:33:58 +0700

    clean data v2

--- analysis/director/director_genres_popular.py
+++ analysis/director/director_genres_popular.py
@@ -4,54 +4,68 @@ import pygal
 from pygal.style import NeonStyle
 
 with open('..\\..\\web_project\\check.json') as genres:
-    DATA = json.load(genres) #import json file
+    DATA = json.load(genres) #import json file(json file is all name genres)
 
 def read():
-    """Read file csv"""
+    """Read file csv and return dataframe"""
     return pd.read_csv('..\\..\\dataset\\tmdb_5000_movies.csv'),\
     pd.read_csv('..\\..\\dataset\\tmdb_5000_credits.csv')
+    #first it is a all dataframe information and second is detail in production 
 
 def memory_director():
-    """function analyse data"""
+    """This function is memory movie of director is directing"""
     df_movie, df_credits = read() #read csv
-    check_dict = {}
-    for idx, data in df_credits.iterrows():
-        for i in json.loads(data.crew):
+    director_movie = {} #This dict is memory name director and his directing movie
+    
+    for idx, data in df_credits.iterrows(): #idx is index of rows and data is data in rows
+        for i in json.loads(data.crew): #data.crew is detail duty
             if i['job'] == 'Director':
                 if i['name'] not in check_dict:
-                    check_dict[i['name']] = [data.movie_id]
+                    director_movie[i['name']] = [data.movie_id]
                 else:
-                    check_dict[i['name']].append(data.movie_id)
-    return check_dict, df_movie, df_credits
+                    director_movie[i['name']].append(data.movie_id)
+        '''This loop is check if i keys job is equal director memory movies name and 
+        name director in dict(director_movie)'''
+    
+    return director_movie, df_movie, df_credits #return dataframe and dict(memory director and movies)
 
 
 def analyse(name):
-    director, df_movie, df_credits = memory_director()
-    check1 = {}
-    for idx, data in df_movie.iterrows():
-        if data.id in director[name]:
-            for i in json.loads(data.genres):
-                if i['name'] in check1:
-                    check1[i['name']] += data.popularity
+    """This function is analyse popular genres of director""" 
+    director, df_movie, df_credits = memory_director() #variable director is name movie of director is directing
+    genres_popular = {} #This variable is memory popularity of genres analytic
+    
+    for idx, data in df_movie.iterrows(): #This loop is in rows movie information 
+        if data.id in director[name]: """data.id is name id movies and is condition is 
+        check name of directing and name movies"""
+            for i in json.loads(data.genres): #data.genres is genres of movie id
+                if i['name'] in genres_popular:
+                    genres_popular[i['name']] += data.popularity #data.popularity is num of popular in movie style
                 else:
-                    check1[i['name']] = data.popularity
-    return check1 #return genres and movie polarity
+                    genres_popular[i['name']] = data.popularity
+    
+    return genres_popular #return genres and movie polarity
 
-def plotgraph(name_director, check):
-    """function plot graph"""
-    popular_genres, count = analyse(name_director), 0
+def plotgraph(name_director, popular_sort):
+    """This function is plot this graph genres popular of director"""
+    popular_genres, num_popular = analyse(name_director), 0
     chart = pygal.Bar(fill=True, interpolate='cubic', style=NeonStyle)
     chart.title = name_director+' Static Popular Genres for his Directing (%)'
-    for i in popular_genres.keys():
-        count += popular_genres[i]
-    for i in popular_genres.keys():
-        if popular_genres[i] not in check:
-            check[popular_genres[i]] = [i]
+    
+    for i in popular_genres.keys(): #This loop is count all num popular
+        num_popular += popular_genres[i]
+    
+    for i in popular_genres.keys(): #This loop is sort data because use data is sort in graph
+        if popular_genres[i] not in popular_sort:
+            popular_sort[popular_genres[i]] = [i]
         else:
-            check[popular_genres[i]].append(i)
-    keys = sorted(check.keys())
+            popular_sort[popular_genres[i]].append(i)
+    
+    keys = sorted(popular_sort.keys()) #keys is list memory keys in popular_sort
+    
     for i in keys:
         for j in check[i]:
-            chart.add(j, round(i/count*100, 4))
+            chart.add(j, round(i/num_popular*100, 4)) #plot graph
     chart.render_to_file('..\\..\\web_project\\static\\svg\\director\\%s\\popular_d.svg'%(name_director.replace(' ', '_')))
+    # render
 plotgraph(input(), {})
